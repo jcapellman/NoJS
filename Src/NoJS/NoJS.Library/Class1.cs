21df6bed2929434ddeac9369372f3c8c93ecb5d2
Jarred Capellman <jcapellman@hotmail.com>
2016-10-11 20:20:53 -0400

    Initial commit

---
+++ Src/NoJS/NoJS.Library/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Encodings.Web;
+
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace NoJS.Library {
+    public class NoJSViewEngine : RazorViewEngine {
+        public NoJSViewEngine(IRazorPageFactoryProvider pageFactory, IRazorPageActivator pageActivator,
+            HtmlEncoder htmlEncoder, IOptions<RazorViewEngineOptions> optionsAccessor, ILoggerFactory loggerFactory)
+            : base(pageFactory, pageActivator, htmlEncoder, optionsAccessor, loggerFactory)
+        {
+        }        
+      
+    }
+}
