02d3294c63bea52aa3980d88a41b54bb7445fddc
Jarred Capellman <jcapellman@hotmail.com>
2016-10-20 20:31:07 -0400

    Added Legacy Filter

--- Src/NoJS/NoJS.Library/Filters/LegacyFilter.cs
+++ Src/NoJS/NoJS.Library/Filters/LegacyFilter.cs
@@ -1,9 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using System;
+using System.IO;
 
 namespace NoJS.Library.Filters {
-    public class LegacyFilter : ActionFilterAttribute {
-        public override void OnResultExecuted(ResultExecutedContext context) {
-            base.OnResultExecuted(context);
+    public class LegacyFilter : Stream {
+        private Stream _shrink;
+
+        public LegacyFilter(Stream shrink) {
+            _shrink = shrink;
+        }
+
+        public override void Flush() {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanRead { get; }
+        public override bool CanSeek { get; }
+        public override bool CanWrite { get; }
+        public override long Length { get; }
+        public override long Position { get; set; }
+
+        public override void Write(byte[] buffer, int offset, int count) {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value) {
+            throw new NotImplementedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin) {
+            throw new NotImplementedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count) {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ Src/NoJS/NoJS.Library/Filters/LegacyFilterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Text;
+
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace NoJS.Library.Filters {
+    public class LegacyFilterAttribute : ActionFilterAttribute {
+        public override void OnResultExecuted(ResultExecutedContext context) {
+            var httpResponse = context.HttpContext.Response;
+            var responseStream = httpResponse.Body;
+
+            responseStream.Seek(0, SeekOrigin.Begin);
+
+            using (var streamReader = new StreamReader(responseStream, Encoding.UTF8, true, 512, true)) {
+                var toCache = streamReader.ReadToEnd();
+                var contentType = httpResponse.ContentType;
+                var statusCode = httpResponse.StatusCode.ToString();
+                
+            }
+
+            base.OnResultExecuted(context);
+        }
+    }
+}
