2f1a652adfcbd6f3c1f1ecd7dfdd8cd645fac0a3
Jarred Capellman <jcapellman@hotmail.com>
2016-10-23 22:05:49 -0400

    Adjusting Filter Attribute

--- Src/NoJS/NoJS.Library/Filters/LegacyFilterAttribute.cs
+++ Src/NoJS/NoJS.Library/Filters/LegacyFilterAttribute.cs
@@ -1,25 +1,17 @@
 ﻿using System.IO;
 using System.Text;
-
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace NoJS.Library.Filters {
     public class LegacyFilterAttribute : ActionFilterAttribute {
-        public override async void OnResultExecuted(ResultExecutedContext context) {
-            var httpResponse = context.HttpContext.Response;
+        public override void OnActionExecuting(ActionExecutingContext filterContext) {
+            base.OnActionExecuting(filterContext);
+
+            var httpResponse = filterContext.HttpContext.Response;
             var responseStream = httpResponse.Body;
-            
-            using (var streamReader = new StreamReader(responseStream, Encoding.UTF8, true, 512, true)) {
-                var toCache = streamReader.ReadToEnd();
-                var contentType = httpResponse.ContentType;
-                var statusCode = httpResponse.StatusCode.ToString();
 
-                while (!streamReader.EndOfStream) {
-                    var line = await streamReader.ReadLineAsync();
-                }                
-            }
-
-            base.OnResultExecuted(context);
+            filterContext.Result = new JsonResult(httpResponse.Body.Read(null, 0, 0));
         }
     }
 }
