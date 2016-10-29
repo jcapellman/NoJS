f4690a772a3d61f5011f740f2d148af04291d596
Jarred Capellman <jcapellman@hotmail.com>
2016-10-28 22:16:13 -0400

    Initial work on js removal

--- Src/NoJS/NoJS.Library/Filters/LegacyMiddleware.cs
+++ Src/NoJS/NoJS.Library/Filters/LegacyMiddleware.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Builder;
@@ -7,13 +8,15 @@ using Microsoft.AspNetCore.Http;
 namespace NoJS.Library.Filters {
     public class LegacyMiddleware {
         private readonly RequestDelegate _next;
+        private bool _enableJS;
 
-        public LegacyMiddleware(RequestDelegate next) {
+        public LegacyMiddleware(RequestDelegate next, bool enableJS = false) {
             _next = next;
+            _enableJS = enableJS;
         }
 
         public async Task Invoke(HttpContext context) {
-            var response = await GenerateResponse(context);
+            var response = await GenerateResponse(context);x
             
             await context.Response.WriteAsync(response);
         }
@@ -31,17 +34,19 @@ namespace NoJS.Library.Filters {
                 newBody.Seek(0, SeekOrigin.Begin);
 
                 content = new StreamReader(newBody).ReadToEnd();
-             
-                //todo implement filtering of legacy functionality
-                   
+
+                if (!_enableJS) {
+                   content = Regex.Replace(content, "<script[^<]*</script>", "");
+                }
+                
                 return content;
             }
         }
     }
 
     public static class LegacyMiddlewareExtensions {
-        public static IApplicationBuilder UseLegacyMiddleware(this IApplicationBuilder builder) {
-            return builder.UseMiddleware<LegacyMiddleware>();
+        public static IApplicationBuilder UseLegacyMiddleware(this IApplicationBuilder builder, bool enableJS = false) {
+            return builder.UseMiddleware<LegacyMiddleware>(enableJS);
         }
     }
 }
